5c2cb47d51a11f0610bab78586806c87f9aa5006
Adam Wolanski <adam.wolanski@protonmail.com>
2022-05-25 15:33:45 +0200

    WIP init window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use vulkano::{
+    device::physical::PhysicalDevice,
+    instance::{Instance, InstanceCreateInfo},
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    let required_extensions = vulkano_win::required_extensions();
+
+    let instance = Instance::new(InstanceCreateInfo {
+        enabled_extensions: required_extensions,
+        ..Default::default()
+    })
+    .unwrap();
+
+    let device = PhysicalDevice::enumerate(&instance).next().unwrap();
+    let event = EventLoop::new();
+
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event, instance)
+        .unwrap();
+
+    
 }
